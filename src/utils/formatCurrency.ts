bd2ede235676c201df7f00987e967ace04352152
Arthur Victor <140616974+arthurvicttor@users.noreply.github.com>
2026-06-05 20:02:13 -0300

    feat: Adicionar funções utilitárias para manipulação de classes e formatação de moeda

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,8 @@
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+}
+
+// Ex: formatCurrency(12.5) → "R$ 12,50"
